4bd2c16f0205e3c98e0ae5f3da8fe5ebdc76c372
bryan.tiban <bryan.tiban@dominioncommercial.es>
2022-10-06 23:21:42 +0200

    creacionFrontal: se añade localización en tiempo real

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
-  await FlutterConfig.loadEnvVariables();
+  Configuration conf = const Configuration();
+  await conf.configureFlutterConfig();
 
-  runApp(const MyApp());
+  runApp(const HomeWidget());
 }
 
+class Configuration {
+  const Configuration();
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  configureFlutterConfig() async{
+    WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
+    await FlutterConfig.loadEnvVariables();
+  }
+
+}
+
+class HomeWidget extends StatefulWidget {
+  const HomeWidget({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  // State<HomeWidget> createState() => _MyAppState(); // lambda style
+  State<StatefulWidget> createState() {
+    return HomeWidgetState();
+  }
+
 }
 
-class _MyAppState extends State<MyApp> {
+class HomeWidgetState extends State<HomeWidget> {
   late GoogleMapController mapController;
+  final LatLng initialCamPosition = const LatLng(40.417015, -3.703205);
+  Location location = Location();
 
-  final LatLng _center = const LatLng(40.417015, -3.703205);
-
-  void _onMapCreated(GoogleMapController controller) {
+  void onMapCreated(GoogleMapController controller) {
     mapController = controller;
+
+    location.onLocationChanged.listen((l) {
+      mapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+          CameraPosition(target: LatLng(l.latitude! , l.longitude!), zoom: 15),
+        ),
+      );
+    });
   }
 
   @override
@@ -32,17 +54,19 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
+          leading: const IconButton(onPressed: null, icon: Icon(Icons.menu_rounded)),
           title: Text(FlutterConfig.get('API_GOOGLE_MAPS_KEY') ?? 'API_GOOGLE_MAPS_KEY not found'),
           backgroundColor: Colors.green[700],
         ),
         body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
-          ),
-        ),
+          initialCameraPosition: CameraPosition(target: initialCamPosition),
+          mapType: MapType.normal,
+          onMapCreated: onMapCreated,
+          myLocationEnabled: true,
+        )
       ),
     );
   }
 }
+
+
