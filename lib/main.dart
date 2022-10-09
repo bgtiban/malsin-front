7ab128fc8dd5e19d187092b500e7ea480128fb28
bgtiban <bgtiban@outlook.es>
2022-10-09 05:44:38 +0200

    creacionFrontal: se añade footer a la pantlla con 3 iconos seleccionables pero sin funcionamiento aún

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_config/flutter_config.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
-
 void main() async {
   Configuration conf = const Configuration();
   await conf.configureFlutterConfig();
@@ -14,11 +13,10 @@ void main() async {
 class Configuration {
   const Configuration();
 
-  configureFlutterConfig() async{
+  configureFlutterConfig() async {
     WidgetsFlutterBinding.ensureInitialized(); // Required by FlutterConfig
     await FlutterConfig.loadEnvVariables();
   }
-
 }
 
 class HomeWidget extends StatefulWidget {
@@ -29,13 +27,13 @@ class HomeWidget extends StatefulWidget {
   State<StatefulWidget> createState() {
     return HomeWidgetState();
   }
-
 }
 
 class HomeWidgetState extends State<HomeWidget> {
   late GoogleMapController mapController;
   final LatLng initialCamPosition = const LatLng(40.417015, -3.703205);
   Location location = Location();
+  int selectedIndex = 0;
 
   void onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -43,9 +41,15 @@ class HomeWidgetState extends State<HomeWidget> {
     location.onLocationChanged.listen((l) {
       mapController.animateCamera(
         CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(l.latitude! , l.longitude!), zoom: 15),
+          CameraPosition(target: LatLng(l.latitude!, l.longitude!), zoom: 15),
         ),
       );
+    });
+  }
+
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
     });
   }
 
@@ -53,20 +57,37 @@ class HomeWidgetState extends State<HomeWidget> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          leading: const IconButton(onPressed: null, icon: Icon(Icons.menu_rounded)),
-          title: const Text("Home"),
-          backgroundColor: Colors.green[700],
-        ),
-        body: GoogleMap(
-          initialCameraPosition: CameraPosition(target: initialCamPosition),
-          mapType: MapType.normal,
-          onMapCreated: onMapCreated,
-          myLocationEnabled: true,
-        )
-      ),
+          appBar: AppBar(
+            leading: const IconButton(
+                onPressed: null, icon: Icon(Icons.menu_rounded)),
+            title: const Text("Inicio"),
+            backgroundColor: Colors.green[700],
+          ),
+          body: GoogleMap(
+            initialCameraPosition: CameraPosition(target: initialCamPosition),
+            mapType: MapType.normal,
+            onMapCreated: onMapCreated,
+            myLocationEnabled: true,
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.camera_alt),
+                label: 'Foto o vídeo',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.report),
+                label: 'Incidencia',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.image),
+                label: 'Buscar foto o vídeo',
+              ),
+            ],
+            currentIndex: selectedIndex,
+            selectedItemColor: Colors.blue,
+            onTap: onItemTapped,
+          )),
     );
   }
 }
-
-
